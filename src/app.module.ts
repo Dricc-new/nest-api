88d379d7352eadc3247f15fbe941c13c0214932a
Dricc-new <diegoramoncc17@gmail.com>
2023-08-23 23:41:13 -0400

    mongoose intalled

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [UsersModule,ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env'
-  })],
+  imports: [UsersModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    })],
 })
 export class AppModule { }
