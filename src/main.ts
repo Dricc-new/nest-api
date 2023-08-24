6c417ac95dbf76df437c963278c9da977a6f5f36
Dricc-new <diegoramoncc17@gmail.com>
2023-08-23 22:58:31 -0400

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan'
+import { CORS } from './constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'))
+  app.setGlobalPrefix('api')
+  app.enableCors(CORS)
+
+  const configService = app.get(ConfigService)
+  await app.listen(configService.get('PORT'));
+  console.log(`App running on: ${await app.getUrl()}`)
+}
+bootstrap();
